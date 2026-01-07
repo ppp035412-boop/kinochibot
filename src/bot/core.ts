00f4b1903e210f4f713976967be318b52745e80d
svkhrobbeck <svkhrobbeck@gmail.com>
2026-01-07 20:08:43 +0500

    feat: :package: add @grammyjs/ratelimiter for limiting telegram bot requests

--- src/bot/core.ts
+++ src/bot/core.ts
@@ -1,4 +1,5 @@
 import { Bot } from "grammy";
+import { limit } from "@grammyjs/ratelimiter";
 
 import handler from "@/bot/handlers";
 import type { BotCTX } from "@/bot/types";
@@ -6,6 +7,14 @@ import { sessionMiddleware } from "@/bot/middlewares";
 
 const bot = new Bot<BotCTX>(process.env.BOT_TOKEN!);
 
+bot.use(
+  limit({
+    limit: 4,
+    timeFrame: 60_000,
+    onLimitExceeded: async ctx => await ctx.reply("Juda ko'p so'rov yubordingiz 1 daqiqadan so'ng urinib ko'ring!"),
+    keyGenerator: ctx => ctx.from?.id.toString(),
+  }),
+);
 bot.use(sessionMiddleware);
 bot.use(handler);
 
