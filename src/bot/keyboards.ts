a24549796750b41cdb42154a37ded2843020a521
svkhrobbeck <svkhrobbeck@gmail.com>
2026-01-07 20:38:21 +0500

    feat: :sparkles: add buildChannelsKeyboard util and make code clean

--- src/bot/keyboards.ts
+++ src/bot/keyboards.ts
@@ -1,3 +1,4 @@
+import type { IChannel } from "@/helpers/interfaces";
 import { InlineKeyboard } from "grammy";
 
 export const adminKeyboard = new InlineKeyboard()
@@ -8,3 +9,19 @@ export const adminKeyboard = new InlineKeyboard()
   .text("Admin uchun havola olish", "query_get_admin_link")
   .row()
   .text("Barcha filmlar ro'yxati", "query_all_movies_list");
+
+export const buildChannelsKeyboard = (channels: IChannel[]) => {
+  const keyboard = new InlineKeyboard();
+
+  if (channels.length === 0) return;
+
+  for (let i = 0; i < channels.length; i++) {
+    const channel = channels[i];
+    if (!channel) continue;
+    const isUrl = channel.username.startsWith("https://t.me/");
+    keyboard.url(`${i + 1}-kanal`, isUrl ? channel.username : `https://t.me/${channel.username}`).row();
+  }
+
+  keyboard.text("✅ Tekshirish", "query_check_subscription");
+  return keyboard;
+};
--- src/bot/utils.ts
+++ src/bot/utils.ts
@@ -1,9 +1,10 @@
 import { get } from "lodash";
-import { Context, InlineKeyboard } from "grammy";
+import { Context } from "grammy";
 
 import bot from "./core";
 import { Admin, Channel } from "@/db/models";
 import { ACCESS_STATUSES } from "@/helpers/constants";
+import { buildChannelsKeyboard } from "@/bot/keyboards";
 
 export const checkIsAdmin = async (userId: number) => {
   const admin = await Admin.findOne({ telegramId: userId }).lean();
@@ -31,19 +32,8 @@ export const checkIsSubscribed = async (userId: number) => {
 };
 
 export const askToSubscribe = async (ctx: Context) => {
-  const keyboard = new InlineKeyboard();
   const channels = await Channel.find({ isActive: true }).lean();
-
-  if (channels.length === 0) return;
-
-  for (let i = 0; i < channels.length; i++) {
-    const channel = channels[i];
-    if (!channel) continue;
-    const isUrl = channel.username.startsWith("https://t.me/");
-    keyboard.url(`${i + 1}-kanal`, isUrl ? channel.username : `https://t.me/${channel.username}`).row();
-  }
-
-  keyboard.text("✅ Tekshirish", "query_check_subscription");
+  const keyboard = buildChannelsKeyboard(channels);
 
   await ctx.reply(`⚠️ Botdan foydalanish uchun quyidagi kanallarga obuna bo'lishingiz shart:\n\nObuna bo'lgach, "✅ Tekshirish" tugmasini bosing.`, { reply_markup: keyboard });
 };
